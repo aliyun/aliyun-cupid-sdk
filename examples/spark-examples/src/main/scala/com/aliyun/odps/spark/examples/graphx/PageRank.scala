9aafac43e27c86825a3eab8e2e81795b3889b727
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2018-02-24 16:11:06 +0800

    add comment on PageRank

--- examples/spark-examples/src/main/scala/com/aliyun/odps/spark/examples/graphx/PageRank.scala
+++ examples/spark-examples/src/main/scala/com/aliyun/odps/spark/examples/graphx/PageRank.scala
@@ -1,12 +1,35 @@
+/**
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements.  See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership.  The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License.  You may obtain a copy of the License at
+  * <p>
+  * http://www.apache.org/licenses/LICENSE-2.0
+  * <p>
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package com.aliyun.odps.spark.examples.graphx
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
+/**
+  * PageRank
+  * 1. build aliyun-cupid-sdk
+  * 2. bin/spark-submit --master yarn-cluster --class com.aliyun.odps.spark.examples.graphx.PageRank
+  * /path/to/aliyun-cupid-sdk/examples/spark-examples/target/spark-examples_2.11-1.0.0-SNAPSHOT-shaded.jar
+  */
 object PageRank {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("pagerank")
+    val conf = new SparkConf().setAppName("PageRank")
     val sc = new SparkContext(conf)
 
     // build vertices
